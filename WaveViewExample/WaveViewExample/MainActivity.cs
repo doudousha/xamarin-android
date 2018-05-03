dd3675d90645cb8d9c72ddb06b310bf1b07a881a
wangqing <wangqing@123>
2018-05-03 23:53:41 +0800

    增加水面波浪形wave view

---
+++ WaveViewExample/WaveViewExample/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Com.John.Waveview;
+
+namespace WaveViewExample
+{
+    [Activity(Label = "WaveViewExample", MainLauncher = true,Theme ="@style/Theme.AppCompat.Light.NoActionBar")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var seekBar = FindViewById<SeekBar>(Resource.Id.seek_bar);
+            var waveView = FindViewById<WaveView>(Resource.Id.wave_view);
+
+            seekBar.ProgressChanged += (sender, eArgs) =>
+            {
+                waveView.SetProgress(seekBar.Progress);
+            };
+        }
+    }
+}
+
