0e7a96fa2593eaa2ab42bd889110abe6e75ef936
wangqing <wangqing@123>
2018-05-04 09:12:59 +0800

    增加底部导航bottomNavigationView

---
+++ BottomNavigationViewExample/BottomNavigationViewExample/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.Design.Widget;
+
+namespace BottomNavigationViewExample
+{
+    [Activity(Label = "BottomNavigationViewExample", MainLauncher = true, Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+
+            var bottomNavigation = FindViewById<BottomNavigationView>(Resource.Id.bv);
+            bottomNavigation.NavigationItemSelected += (sender, eArgs) => {
+                switch (eArgs.Item.ItemId) {
+                    case Resource.Id.item_add:
+                        Toast.MakeText(this, "action add clicked", ToastLength.Short).Show();
+                        break;
+                    case Resource.Id.item_edit:
+                        Toast.MakeText(this, "action edit clicked", ToastLength.Short).Show();
+                        break;
+                    case Resource.Id.item_delete:
+                        Toast.MakeText(this, "action delete clicked",ToastLength.Short).Show();
+                        break;
+                }
+            };
+        }
+    }
+}
+
